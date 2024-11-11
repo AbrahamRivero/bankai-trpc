17bdcef77e3e89d3e5ff82c5dce786df3bc45142
AbrahamRivero <riveromartinezabraham@gmail.com>
2024-11-10 22:20:25 -0500

    feat: :art: Working on Clerk Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,21 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/shopping-cart(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/admin(.*)"]);
 
-const isProtectedAdminRoute = createRouteMatcher(["/dashboard(.*)"]);
-
-export default clerkMiddleware((auth, request) => {
+export default clerkMiddleware(async (auth, request) => {
+  const { has, redirectToSignIn } = await auth();
   // Restrict admin routes to users with specific permissions
-  /*   if (isProtectedAdminRoute(request)) {
-    auth().protect((has) => {
-      return has({ role: "admin" });
-    });
-  } */
-  // Restrict organization routes to signed in users
-  if (isProtectedRoute(request)) auth().protect();
+
+  if (isProtectedRoute(request)) {
+    await auth.protect();
+
+    return redirectToSignIn();
+  }
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
